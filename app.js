18a62b468cc3a4047d8ba4638dea6ca7f4a6cdd8
Negar Sarani <negar.sarani2000@gmail.com>
2023-01-29 01:54:27 +0330

    write code for Question3

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 "use stirct"
+const array = ["Hare", "Krishna", "Hare", "Krishna", "Krishna", "Krishna", "Hare", "Hare", ":-O"];;
+
+
+function uniqueArray(array) {
+    const createSet = new Set(array);
+    console.log(createSet);
+}
+uniqueArray(array)
