6245f28c5b97a8146c8346ae130f2523711eb0a0
Negar Sarani <negar.sarani2000@gmail.com>
2023-01-30 01:36:07 +0330

    write code question4

--- app.js
+++ app.js
@@ -1,48 +1,16 @@
 "use stirct"
 
-
-
-// console.log(arr[2].length);
-// let arr1 = []
-// let saveIndex = []
-
-// function aclean(item) {
-//     const split = arr[0]
-
-//     for (const x of split) {
-//         saveIndex.push(x);
-
-//         for (let i = 0; i <= arr.length; i++) {
-//             if (arr[i].length === saveIndex.length) {
-//                 // console.log(i);
-
-
-//             }
-
-
-//         }
-
-//     }
-
-// }
-// console.log(saveIndex);
-// console.log(aclean(arr));
-
-// تعداد حروف یکسان  === 
-//حروف یکسان === اما ترتیب متفاوت است
-
-let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
-
-// function aclean(item) {
-//     const lentghArr = item.map(a => a.length)
-//     console.log(lentghArr);
-
-//     for (let i = 0; i <= arr.length; i++) {
-//         if(lentghArr[i] === arr[0].length){
-//             arr[0]
-//         }
-//     // const element = array[index];
-    
-//    }
-// }
-// aclean(arr)
+function aclean(arr) {
+    let map = new Map();
+  
+    for (let word of arr) {
+      let sorted = word.toLowerCase().split('').sort().join('');
+      map.set(sorted, word);
+    }
+  
+    return Array.from(map.values());
+  }
+  
+  let arr = ["nap", "teachers", "cheaters", "PAN", "ear", "era", "hectares"];
+  
+  alert( aclean(arr) );
