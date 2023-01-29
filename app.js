0822161b1e0a59c57de50aa293c69cbe864bc403
Negar Sarani <negar.sarani2000@gmail.com>
2023-01-29 14:25:06 +0330

    write test1 code

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 "use stirct"
+let map = new Map();
+map.set("name", "John");
+
+let keys = [...map.keys()];
+
+console.log(keys);
+keys.push("more" , "less");
+
+
+const [...arrayMap] = map
+console.log(arrayMap);
+arrayMap.push(keys)
+
+
+
+//To solve the error, convert the value to an array before calling the method, or make sure to only call the push() method on valid arrays.
