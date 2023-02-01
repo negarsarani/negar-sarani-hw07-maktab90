feec80ba275adc704d27b54230938ac1cffa5fff
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-01 11:00:17 +0330

    add feature Drag scroll to the header filter

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
 " use strict"
+const slider = document.querySelector('.header-filter');
+let mouseDown = false;
+let startX, scrollLeft;
+
+let startDragging = function (e) {
+    mouseDown = true;
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+  };
+  let stopDragging = function () {
+    mouseDown = false;
+  };
+  
+  slider.addEventListener('mousemove', (e) => {
+    e.preventDefault();
+    if(!mouseDown) { return; }
+    const x = e.pageX - slider.offsetLeft;
+    const scroll = x - startX;
+    slider.scrollLeft = scrollLeft - scroll;
+  });
+  
+  // Add the event listeners
+  slider.addEventListener('mousedown', startDragging, false);
+  slider.addEventListener('mouseup', stopDragging, false);
+  slider.addEventListener('mouseleave', stopDragging, false);
