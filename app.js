b4d07e0774ae06e209d6c5ba2db2db2d756f3354
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-02 01:20:48 +0330

    add obj

--- app.js
+++ app.js
@@ -5,21 +5,32 @@ document
 const form = document.querySelector(".form");
 const emptyValue = document.querySelectorAll(".empty-value");
 const notice = document.querySelectorAll(".notice");
-const password = document.querySelector(".password");
-const RePassword = document.querySelector(".RePassword");
 const checkPass = document.querySelector(".checkPass");
 
-function checkPassword(Event) {
+const firstName = document.querySelector(".input-firstName");
+const Middlename = document.querySelector(".input-Middlename");
+const Lastname = document.querySelector(".input-Lastname");
+const phone = document.querySelector(".phoneInput");
+const phoneNo = document.querySelector(".input-phoneNo");
+const currentAdress = document.querySelector(".input-Current-Adress");
+const email = document.querySelector(".input-email");
+const password = document.querySelector(".password");
+const RePassword = document.querySelector(".RePassword");
+
+
+function checkPassword() {
   password.value == RePassword.value
     ? checkPass.classList.add("display-notice")
     : checkPass.classList.remove("display-notice");
 }
 
+
 function registerbtn(Event) {
   const array = [...emptyValue];
   Event.preventDefault();
+  
   const mapInput = array.map((a) => a.value);
-
+  
   for (let index = 0; index < mapInput.length; index++) {
     checkPassword();
     if (mapInput[index] !== "" || mapInput[index].length !== 0) {
@@ -30,4 +41,16 @@ function registerbtn(Event) {
       notice[index].classList.remove("display-notice");
     }
   }
+  let obj={
+    firstName : firstName.value,
+    Middlename : Middlename.value,
+    Lastname : Lastname.value,
+    phone : phone.value,
+    phoneNo : phoneNo.value,
+    currentAdress : currentAdress.value,
+    email : email.value,
+    password : password.value,
+    RePassword : RePassword.value,
+  } 
+  console.log(obj);
 }
