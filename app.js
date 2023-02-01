d3685a2f33c8aa66569bb001e6492ac098163a7c
Negar Sarani <negar.sarani2000@gmail.com>
2023-02-01 23:16:31 +0330

    add sort function

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 "use strict"
-let arrayInput = [8, 3, 6, 1];
-let arrayInput1 = [5, 10, 4, 15];
-
-const getInputs = [...arrayInput, ...arrayInput1]
-console.log(getInputs.sort((a, b) => {
-    return a - b
-}));
+function sort(arrayInput , arrayInput1){
+  const getInputs = [...arrayInput, ...arrayInput1]
+ return getInputs.sort((a, b) => {
+      return a - b
+  })
+}
+console.log(sort([8,3,6,1],[4,5,2]));
 
 
